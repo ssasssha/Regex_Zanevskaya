85c6d7cd7bc047f1772a8afc336575142d9add14
crumb <yanguzovgr@gmail.com>
2023-11-13 05:52:55 +0500

    Создание классов. Заневская

---
+++ Classes/Common/CheckRegex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Zanevskaya.Classes.Common
+{
+    public class CheckRegex
+    {
+        /// <param name="Pattern">Регулярное выражение</param> 
+        /// <param name="Input">Строка ввода</param>
+        /// <returns></returns>
+        public static bool Match(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+    }
+}
